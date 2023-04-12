b10412d178ea4ef03def12e85d83af3944ae0a78
niksync1 <40718182+niksync1@users.noreply.github.com>
2023-04-12 14:00:02 +0000

    fetching data from job site with api

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -6,23 +6,26 @@ import { useRouter} from 'expo-router'
 import styles from './popularjobs.style'
 import {COLORS, SIZES } from '../../../constants';
 import PopularJobCard from '../../common/cards/popular/PopularJobCard';
+import useFetch from '../../../hook/useFetch';
 
 
 const Popularjobs = () => {
   const router =useRouter();
-  const isLoading = false;
-  const error = false;
+  const {data, isLoading, error} = useFetch('search', {
+    query: 'React developer',
+    num_pages: 1,
+  })
+
+  console.log(data);
 
   return (
     <View style={styles.container}>
-
       <View style={styles.header}>
         <Text style={styles.headerTitle}>Popular Jobs</Text>
         <TouchableOpacity>
           <Text style={styles.headerBtn}>Show all</Text>
         </TouchableOpacity>
       </View>
-
       <View styles={styles.cardsContainer} >
         {isLoading ? (
           <ActivityIndicator size="large" colors={COLORS.primary} />
@@ -31,18 +34,17 @@ const Popularjobs = () => {
         ) : (
           <FlatList
             data ={ [ 1, 2, 3, 4, 5, 6, 7, 8]}
-            renderItem={({item}) => (
+            renderItem={({ item }) => (
               <PopularJobCard
                 item={item} 
               />
             ) }
             keyExtractor={ item => item?.job_id}
-            contentContainerStyle={{ columnGap: SIZES.medium}}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
             horizontal
            />
-        ) }
+        )}
       </View>
-
     </View>
   )
 }
